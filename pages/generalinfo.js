b023c915585bbc87b4061d4a2e0c6a8c69034956
koalascode <90786950+koalascode@users.noreply.github.com>
2022-03-30 18:58:47 -0400

    changes made to things

--- pages/generalinfo.js
+++ pages/generalinfo.js
@@ -8,7 +8,7 @@ export default function General() {
     return (
         <div className={styles.container}>
             <div>
-                <h3>Hallucinagenic drugs change a person's percentions of reality. They are sometimes refered to as "psychodelic drugs" as they can make people feel, see, and hear things that are not real, anr/or distort their understandings of what is real.</h3>
+                
             </div>
         </div>
     )
