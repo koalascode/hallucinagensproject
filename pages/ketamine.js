d40af1247c8d894c49ab3782795024c101004d02
koalascode <90786950+koalascode@users.noreply.github.com>
2022-03-31 15:41:12 -0400

    added points to ketamine

--- pages/ketamine.js
+++ pages/ketamine.js
@@ -29,7 +29,9 @@ export default function Ketamine() {
                     </li>
                     <li data-icon="💊 " className={styles.listitemtext}>It is injected as a fluid and made into powders which are snorted, mixed in drinks, or smoked. </li>
                     <li data-icon="💊 " className={styles.listitemtext}>It gives hallucinatory effects that last 30-60 minutes. It distorts sight and sound, induces feelings of calmness and relaxation as well as relief from pain. It causes “trips” and that makes it used as a party drug. Trips can last about 2 hours.</li>
-                    
+                    <li data-icon="💊 " className={styles.listitemtext}>The only FDA approved form for ketamine is as an antidepressant nasal spray called esketamine.</li>
+                    <li data-icon="💊 " className={styles.listitemtext}>Ketamine is usually produced in the US, but it is made in a number of countries. A large portion of the illicit ketamine trade in the US is sourced from legal uses of the drug, as dealers may smuggle ketamine from veterinarians or from across the Mexican border.</li>
+                    <li data-icon="💊 " className={styles.listitemtext}>Ketamine is made via chemical processes in labs. </li> 
                 </ul>
             </div>
                 
