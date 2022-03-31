65f6e6a66713d25ea9915bab08cff274a4d2cbbe
koalascode <90786950+koalascode@users.noreply.github.com>
2022-03-31 13:18:15 -0400

    PCP page finished

--- pages/pcp.js
+++ pages/pcp.js
@@ -32,21 +32,22 @@ export default function PCP() {
 
                         </ul>
                     </li>
-                    <li data-icon="💊 " className={styles.listitemtext}>Frequently used on blotter paper. It is also often used in tablets known as microdots, sugar cubes, or in liquid.
+                    <li data-icon="💊 " className={styles.listitemtext}>PCP is known as a mind-altering drug, meaning that it will affect your brain and cause shifts in your mood and actions.
                         <ul>
-                            <li className={styles.listitemtext}>Despite its many forms, LSD is almost always consumed orally. With blotter paper, it is put under or on the tongue and then sucked on as the drug begins to take effect.</li>
+                            <li className={styles.listitemtext}>PCP&apos;s subsequent hallucinations affect your sight and vision. PCP is also a dissociative drug, meaning it makes you feel disconnected from the world, and it can cause a euphoric feeling during the high.</li>
+                            <li className={styles.listitemtext}></li>
                         </ul>
                     </li>
-                    <li data-icon="💊 " className={styles.listitemtext}>LSD is most often produced in illicit, underground laboratories across the US.</li>
-                    <li data-icon="💊 " className={styles.listitemtext}>People who ingest LSD do so in order to experience mood shifts and visual hallucinations. During periods of hallucination, users of the drug will experience impacted perception of both time and their surroundings.</li>
-                    <li data-icon="💊 " className={styles.listitemtext}>If users overdose, they will experience more severe and longer trips, and occasionally consequences are fatal.</li>
-                    <li data-icon="💊 " className={styles.listitemtext}>“The ability to make sound judgments and see common dangers is impaired, making the user susceptible to personal injury” (DEA).</li>
+                    <li data-icon="💊 " className={styles.listitemtext}>PCP is usually a white powder that is often in a solution of alcohol or water, and it can be bought in both powder and liquid form. It may also be bought as a tablet or capsule.</li>
+                    <li data-icon="💊 " className={styles.listitemtext}>PCP is ingested via snorting, injection, smoking, and oral consumption.</li>
+                    <li data-icon="💊 " className={styles.listitemtext}>PCP is made in illicit laboratories, with most coming from Southern California. There is also pharmaceutical PCP that has made its way into the illegal drug market. Nonetheless, it is still being used medically in veterinary practices.</li>
+                    <li data-icon="💊 " className={styles.listitemtext}>PCP works by restricting the movement of certain chemicals in a user&apos;s brain.</li>
                 </ul>
             </div>
                 
             <div className={styles.riskscontainer}>
                 <h1 className={styles.centeredsectiontwo}>Health Risks</h1>
-                <p className={styles.risksmain}>The major health risks of LSD are something something something something something something something something something. Something something something something something something something something something something something something something something.</p> 
+                <p className={styles.risksmain}>PCP can cause much numbness throughout a user’s body, and anxious and violent tendencies may result from larger doses. PCP may also cause a rise in heart rate, and due to its analgesic effects, users won’t feel pain if they are severely injured during a high. Long-term health risks include memory loss and difficulty thinking, and speech may become unclear for prolonged users of the drug.</p> 
             </div>
 
             <Footer />
